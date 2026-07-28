d279544b751ce6dc045f65451d74eb7c3b2da83c
Mohammed Haneen P M <mohammedhaneen.pm2003@gmail.com>
2026-07-28 19:38:06 +0530

    Updates

---
+++ _next/static/s4UTlbt8mCwOs2DErV69Z/_buildManifest.js
@@ -0,0 +1,16 @@
+self.__BUILD_MANIFEST = {
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/Appolo-Sports-Demo//_next/:path+",
+        "destination": "/Appolo-Sports-Demo/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/_app",
+    "/_error"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
